baa685a21caa656734fec00d17b78318547bb60a
A . S . P . I <35984842+roookieengineer@users.noreply.github.com>
2019-06-17 15:47:46 +0700

    Update 1-helloworld.go

--- 1-helloworld.go
+++ 1-helloworld.go
@@ -1,7 +1,10 @@
-package main
+package main //akan dieksekusi pertama kali ketika program dijalankan
 
-import "fmt"
+import "fmt" 
+//digunakan untuk import package 
+//fmt adalah package yang digunakan untuk melakukan I/O berupa text
 
-func main(){
+func main(){ 
   fmt.Println("Hello World")
+  //fmt.Println() digunakan untuk memunculkan text ke layar
 }
