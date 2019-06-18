33644722da184a170b08378a44f5e2253f1f434c
A . S . P . I <35984842+roookieengineer@users.noreply.github.com>
2019-06-18 08:23:59 +0700

    Create 2-variabel.go

---
+++ 2-variabel.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main(){
+  //deklarasi variabel 1
+  var firstName string = "Roookie"
+  
+  // deklarasi variabel 2
+  var lastName string
+  lastName = "Engineer
+  
+  
+  //deklarasi variabel 3
+  aka := "A . S . P . I"
+  
+  fmt.Printf("Hello My name is %s %s as knows as %s", firstName, lastName, aka)
+}
