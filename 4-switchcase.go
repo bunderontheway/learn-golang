13cea1a8d0ea348f1b11f1cc50068f41b3c21f57
A . S . P . I <35984842+roookieengineer@users.noreply.github.com>
2019-06-18 09:36:39 +0700

    Create 4-switchcase.go

---
+++ 4-switchcase.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main(){
+  var score = 7
+  
+  switch score {
+  case 10 : 
+    fmt.Println("Excellent")
+  case 8 :
+    fmt.Println("Good Job")
+  case 6 :
+    fmt.Println("NICE Try")
+  case 4 :
+    fmt.Println("Try Again")
+}
