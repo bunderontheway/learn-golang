d50275578a692c65266cacd35cd0f08b4f363004
A . S . P . I <35984842+roookieengineer@users.noreply.github.com>
2019-06-19 09:27:11 +0700

    Update and rename 5-for.go to 6-for.go

--- 6-for.go
+++ 6-for.go
@@ -3,7 +3,17 @@ package main
 import "fmt"
 
 func main(){
+  
+  //cara 1
   for i := 0 ; i < 5 ; i++{
     fmt.Println("Angka" , i)
+  }
+  
+  //cara 2
+  var j = 0
+  
+  for j < 10{
+    fmt.Println("Angka" , j)
+    j++
   }
 }
