e0a900720ba1d75f4cadd7e70f4eeb9899c0ce64
A . S . P . I <35984842+roookieengineer@users.noreply.github.com>
2019-12-23 19:55:21 +0700

    Update 4-switchcase.go

--- 4-switchcase.go
+++ 4-switchcase.go
@@ -1,18 +1,22 @@
 package main
 
-import "fmt"
+import "fmt" // Package untuk melakukan input / output
 
 func main(){
-  var score = 7
+  var score = 7 // Deklarasi variabel score
   
-  switch score {
-  case 10 : 
-    fmt.Println("Excellent")
-  case 8 :
-    fmt.Println("Good Job")
-  case 4 :
-    fmt.Println("Try Again")
-  default :
-    fmt.Println("Not Bad")
+  switch score { // Mengambil kondisi dari variabel score
+    
+  case 10 : // Jika nilai dari score adalah 10
+    fmt.Println("Excellent") // Maka ini yang akan ditampilkan
+    
+  case 8 : // Jika nilai dari score adalah 8
+    fmt.Println("Good Job") //Maka ini yang akan ditampilkan
+    
+  case 4 : // Jika nilai dari score adalah  4
+    fmt.Println("Try Again") // Maka ini yang akan ditampilkan
+    
+  default : // Default adalah kondisi jika nilai dari score tidak ada di salah satu point tersebut
+    fmt.Println("Not Bad") // Maka ini yang akan ditampilkan
   }
 }
