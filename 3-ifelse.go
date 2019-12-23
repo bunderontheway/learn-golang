1a55e7bbfe35759da6f3085420d51880e72b7dd8
A . S . P . I <35984842+roookieengineer@users.noreply.github.com>
2019-12-23 19:51:24 +0700

    Update 3-ifelse.go

--- 3-ifelse.go
+++ 3-ifelse.go
@@ -1,18 +1,21 @@
 package main
 
-import "fmt"
+import "fmt" // package untuk melakukan input / output
 
 func main() {
-  var nilai = 80
+  var nilai = 80 // Deklarasi variabel
   
-  if nilai == 100{
-    fmt.Println("Nilai anda A")  
-  }else if nilai > 75{
-    fmt.Println("Nilai anda B")
-  }else if nilai >= 50 && nilai <= 75 {
-    fmt.Println("Nilai anda C")
-  }else {
-    fmt.Println("Anda harus mengulang")
+  if nilai == 100{ //Jika variabel nilai yang dideklarasikan == 100
+    fmt.Println("Nilai anda A")  // Maka ini yang akan ditampilkan
+    
+  }else if nilai > 75{ //Jika variabel nilai yang dideklarasikan > 75
+    fmt.Println("Nilai anda B") // Maka ini yang akan ditampilkan
+    
+  }else if nilai >= 50 && nilai <= 75 { // Jika variabel nilai yang dideklarasikan >= 50 dan <= 75
+    fmt.Println("Nilai anda C") // Maka ini yang akan ditampilkan
+    
+  }else { // Jika variabel nilai tidak berisi satupun dari ketiga poin diatas
+    fmt.Println("Anda harus mengulang") // Maka ini yang akan ditampilkan
   }
   
 }
