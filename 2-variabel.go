b6a9b9d82cd27143e99596c30ead9e6eedc2ad84
A . S . P . I <35984842+roookieengineer@users.noreply.github.com>
2019-12-23 19:47:16 +0700

    Update 2-variabel.go

--- 2-variabel.go
+++ 2-variabel.go
@@ -3,15 +3,15 @@ package main
 import "fmt"
 
 func main(){
-  //deklarasi variabel 1
+  // cara deklarasi variabel 1
   var firstName string = "Roookie"
   
-  // deklarasi variabel 2
+  // cara deklarasi variabel 2
   var lastName string
   lastName = "Engineer
   
   
-  //deklarasi variabel 3
+  // cara deklarasi variabel 3
   aka := "A . S . P . I"
   
   fmt.Printf("Hello My name is %s %s as knows as %s", firstName, lastName, aka)
