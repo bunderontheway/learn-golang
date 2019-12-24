693f31be7d673415649927358d116eeb8488317c
A . S . P . I <35984842+roookieengineer@users.noreply.github.com>
2019-12-24 09:35:17 +0700

    Update 5-simplesumcalc.go

--- 5-simplesumcalc.go
+++ 5-simplesumcalc.go
@@ -1,16 +1,22 @@
 package main
 
-import "fmt"
+import "fmt" // Package untuk melakukan input / output
 
 func main(){
-  fmt.Print("Masukkan angka pertama  :")
-  var angkaPertama int
-  fmt.Scanln(&angkaPertama)
-  fmt.Print("\n Masukkan angka kedua    :")
-  var angkaKedua int
-  fmt.Scanln(&angkaKedua)
   
-  var hasilJumlah int = angkaPertama + angkaKedua
+  fmt.Print("Masukkan angka pertama  :") // Menampilkan string
+  var angkaPertama int // Deklarasi variabel angkaPertama sebagai integer
+  fmt.Scanln(&angkaPertama) // Fungsi untuk menyimpan input yang akan disimpan di angkaPertama
   
-  fmt.Print("\n Hasil Penjumlahan   : %i ", hasilJumlah)
+  fmt.Print("\n Masukkan angka kedua    :") // Menampilkan string
+  var angkaKedua int // Deklarasi variabel angkaKedua sebagai integer
+  fmt.Scanln(&angkaKedua) // Fungsi untuk menyimpan input yang akan disimpan di angkaKedua
+  
+  var hasilJumlah int = angkaPertama + angkaKedua 
+  // Deklarasi variabel hasilJumlah sebagai integer
+  // Berisi penjumlahan dari angkaPertama dan angkaKedua yang telah diinputkan
+  
+  fmt.Print("\n Hasil Penjumlahan   : %i ", hasilJumlah) 
+  //Menampilkan string, %i untuk menampilkan integer dari variabe hasilJumlah
+  
 }
