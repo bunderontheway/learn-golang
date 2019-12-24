8c0f8ea1408a9e6239ead1a1bc980c0a01cd7c54
A . S . P . I <35984842+roookieengineer@users.noreply.github.com>
2019-12-24 10:50:13 +0700

    Create 10-array1.go

---
+++ 10-array1.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt" // Package untuk melakuka input / output
+
+func main {
+
+var nama [5] string // Deklarasi variabel nama sebagai array yang berisi 5 indeks
+
+// isi dari setiap indeks pada variabel nama, pada array dimulai dari indeks ke-0
+
+nama [0] = "budi" //isi indeks ke-0
+nama [1] = "anduk" //isi indeks ke-1
+nama [2] = "rudi" //isi indeks ke-2
+nama [3] = "tabudi" //isi indeks ke-3
+nama [4] = "snape" //isi indeks ke-4
+
+// Menampilkan string, dan isi dari variabel nama
+fmt.Println("Nama siswa yang remidi adalah" , nama [0], nama [1], nama [2], nama [3], nama [4])
+
+
+}
