126573d52d06f56d174a4ae2b270d94a9009a422
A . S . P . I <35984842+roookieengineer@users.noreply.github.com>
2019-12-24 11:03:09 +0700

    Create 11-array2.go

---
+++ 11-array2.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main{
+
+var binatang [5] string // Deklarasi variabel binatang 
+
+// Deklarasi elemen array secara horizontal
+binatang = [5] string {"semut", "tapir", "trenggiling", "lalat", "cicak"} 
+
+// Menampilkan isi dari elemen array 
+fmt.Println(binatang)
+
+}
